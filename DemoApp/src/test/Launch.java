d39af75edc86c2b2e63786194d8eb36493d53e97
Arpit6677 <arpitbawane.sk@gmail.com>
2023-02-08 14:01:05 +0530

    First commit

---
+++ DemoApp/src/test/Launch.java
@@ -0,0 +1,17 @@
+package test;
+
+public class Launch {
+	public static void main(String[] args) {
+		
+		
+		String[] ar = { "strd", "dhsggd" };
+		for (String str : ar) {
+			System.out.println(str);
+		}
+
+		int i = 20;
+		int j = 22;
+		int c = i + j;
+		System.out.println(c);
+	}
+}
